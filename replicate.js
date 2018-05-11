c9f606819fd899929185cc885d928a76b4e7756e
Johannes J. Schmidt <schmidt@netzmerk.com>
2018-05-11 19:25:15 +0200

    fix: replicate by revision

--- replicate.js
+++ replicate.js
@@ -1,7 +1,7 @@
 module.exports = (source, target) => {
   return source.allDocs()
     .then(docs => {
-      const ids = docs.map(doc => ({ id: doc._id }))
+      const ids = docs.map(doc => ({ id: doc._id, rev: doc._rev }))
 
       return source.bulkGet(ids, { revs: true })
     })
